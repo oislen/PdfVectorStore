8e63ae10335402a5076e2cb0a75a221f9903dc71
Oisin <oisin.leonard@gmail.com>
2024-08-25 12:13:12 +0100

    Created .bat file for creating pdf vector store conda environment

---
+++ environment/pdfvectorstore.bat
@@ -0,0 +1,21 @@
+:: list all available conda environments
+call conda env list
+
+:: create and activate new environment
+call conda env remove --name pdfvectorstore
+call conda env list
+call conda create --name pdfvectorstore python=3.6 --yes
+call conda activate pdfvectorstore
+call conda list
+
+:: install all relevant python libraries
+call pip install -r ..\requirements.txt
+
+:: list all installed libraries
+call conda list
+
+:: export to yml file
+:: call conda env export > pdfvectorstore.yml
+:: call conda env create -f pdfvectorstore.yml
+
+::call conda deactivate
