e50661604c2a6e2eb5cece3c5ba3fac913c4ed2f
Oisin <oisin.leonard@gmail.com>
2024-09-01 09:38:25 +0000

    Updated cons.py sections and added brower links

--- scripts/cons.py
+++ scripts/cons.py
@@ -1,12 +1,22 @@
 import os
 import sys
 
-root_dir = "/home/ubuntu" if sys.platform == "linux" else "E:\\GitHub\\PdfVectorStore"
+# directory settings
+root_dir = "/home/ubuntu/PdfVectorStore" if sys.platform == "linux" else "E:\\GitHub\\PdfVectorStore"
 cred_dir = os.path.join(root_dir, ".cred")
 data_dir = os.path.join(root_dir, "data")
 pdf_fpath = os.path.join(data_dir, "1.pdf")
+
+# poppler and tesseract settings
+poppler_path = None if sys.platform == "linux" else 'C:\\poppler-23.11.0\\Library\\bin'
+tesseract_exe_fpath = None if sys.platform == "linux" else 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+dpi = 500
+
+# elastic and kibana settings
+elastic_localhost_url = "https://host.docker.internal:9200" if sys.platform == "linux" else "https://localhost:9200"
 elastic_docker_ca_crt_fpath = os.path.join(cred_dir, "http_ca.crt")
 elastic_docker_cred_fpath = os.path.join(cred_dir, "elastic.json")
-tesseract_exe_fpath = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-elastic_localhost_url="https://localhost:9200"
 elastic_index_name = "pdfvectorstore"
+elastic_request_timeout = 10
+browser_kibana_url = "http://localhost:5601/app/home#/"
+browser_elastic_url = "https://localhost:9200"
