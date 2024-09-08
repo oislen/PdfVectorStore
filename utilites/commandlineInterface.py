d1b4d91c5401bedeec644c76b5b6a0f797c9d54e
Oisin <oisin.leonard@gmail.com>
2024-09-08 19:40:50 +0100

    Added elastic_index_name parameter to commandlineInterface

--- utilites/commandlineInterface.py
+++ utilites/commandlineInterface.py
@@ -13,6 +13,7 @@ def commandlineInterface():
     Parameters
     
     --operation
+    --elastic_index_name
     --pdf_fpath
     --text
     
@@ -33,12 +34,14 @@ def commandlineInterface():
     # add input arguments
     parser.add_argument("--operation", action = "store", dest = "operation", default = None, type = str, help = "ETL operation.")
     parser.add_argument("--pdf_fpath", action = "store", dest = "pdf_fpath", default = None, type = str, help = "Pdf file path.")
+    parser.add_argument("--elastic_index_name", action = "store", dest = "elastic_index_name", default = None, type = str, help = "The target elastic index.")
     parser.add_argument("--text", action = "store", dest = "text", default = None, type = str, help = "Query text.")
 
     # extract input arguments
     args = parser.parse_args()
     operation = args.operation
     pdf_fpath = args.pdf_fpath
+    elastic_index_name = args.elastic_index_name
     text  = args.text
     
-    return operation, pdf_fpath, text
+    return operation, pdf_fpath, elastic_index_name, text
