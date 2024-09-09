441bb48665ff9f3dca50c0e71c0688f60f843a0b
Oisin <oisin.leonard@gmail.com>
2024-09-09 09:35:56 +0100

    Added full docker command option names. Added example docker run command for calling lambda handler

--- exePdfDocker.bat
+++ exePdfDocker.bat
@@ -6,20 +6,22 @@ SET DOCKER_IMAGE=%DOCKER_USER%/%DOCKER_REPO%:%DOCKER_TAG%
 SET DOCKER_CONTAINER_NAME=pdf01
 
 :: remove existing docker containers and images
-:: docker container prune -f
-docker container rm -f %DOCKER_CONTAINER_NAME%
-docker rm -f %DOCKER_IMAGE%
+:: docker container prune --force
+docker container rm --force %DOCKER_CONTAINER_NAME%
+docker rm --force %DOCKER_IMAGE%
 
 :: Create a new docker network.
 call docker network create elastic
 
 :: build docker image
-call docker build --no-cache -t %DOCKER_IMAGE% . 
-::call docker build -t %DOCKER_IMAGE% .
+call docker build --no-cache --tag %DOCKER_IMAGE% . 
+::call docker build --tag %DOCKER_IMAGE% .
 
 :: run docker container
 SET UBUNTU_DIR=/home/ubuntu
-call docker run  --name %DOCKER_CONTAINER_NAME% --net elastic -p 8501:8501 -it %DOCKER_IMAGE%
+call docker run  --name %DOCKER_CONTAINER_NAME% --net elastic --publish 8501:8501 -it %DOCKER_IMAGE%
 
 :: copy credential files to docker container
 call docker cp .cred %DOCKER_CONTAINER_NAME%:/home/ubuntu/PdfVectorStore/.cred
+ 
+:: call docker run --net elastic --publish 8501:8501 --rm %DOCKER_IMAGE%  --operation query_index --elastic_index_name pdfvectorstore --text Musterkunde
